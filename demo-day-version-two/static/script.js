d2bed4e259896c79aea96577f2b1568d6f190ffd
Isaiah Lowe-Brown <isaiahlowebrown@gmail.com>
2022-12-07 15:10:33 -0500

    codename: mission accomplish 1.0

--- demo-day-version-two/static/script.js
+++ demo-day-version-two/static/script.js
@@ -16,22 +16,64 @@ async function renderChart() {
   // render the chart into a container
   chart
     .render(document.getElementById('chart'))
-  
-    // window.onload=function(){
-    //   document.getElementById('refreshButton').addEventListener('click', () => chart.refresh());
-    // }
-  
-  document.getElementById("refresh").addEventListener("click", () => {
-    chart
-    .render(document.getElementById('chart'))
-  });
+
+  //refreshes page 
+    window.onload=function(){
+      document.getElementById('refresh').addEventListener('click', () => chart.refresh());
+    }
+    
+    let element = document.querySelector(".body-content")
+
+    // document
+    // .getElementById("themeSwitch")
+    // .addEventListener("change", async function () {
+    //   if (this.checked) {
+    //     await chart.setTheme('dark');
+    //     element.classList.toggle("dark-mode", true);
+    //   } else {
+    //     await chart.setTheme('light');
+    //     element.section.classList.toggle("dark-mode", false);
+    //   }
+
+    //   // var currentTheme = await chart.getTheme();
+    //   // document.getElementById("currentTheme").innerText = currentTheme;
+    // });
+
   //filtering 
   const gameSelect = document.getElementById("game-filter")
   gameSelect.addEventListener("change", async (e) => {
     const gameSelect = e.target.value;
     console.log(gameSelect)
     gameSelect ? chart.setFilter({ game : {'$oid': gameSelect} }) : chart.setFilter({});
+    
   });
+
+
+  document
+    .getElementById("chartDataButton")
+    .addEventListener("click", async function () {
+      const chartDataButtonTextElement = document.getElementById(
+        "chartDataButtonText"
+      );
+      const jsonViewerElement = document.getElementById("jsonViewer");
+
+      const currentState = chartDataButtonTextElement.textContent;
+      if (currentState === "Show") {
+        // Update text inside button
+        chartDataButtonTextElement.innerText = "Hide";
+
+        // Get chart data and display it
+        const data = await chart.getData();
+        jsonViewerElement.data = data;
+        jsonViewerElement.style.visibility = "visible";
+      } else {
+        // Update text inside button
+        chartDataButtonTextElement.innerText = "Show";
+        // Reset JSON Viewer element
+        jsonViewerElement.data = "{}";
+        jsonViewerElement.style.visibility = "hidden";
+      }
+    });
 }
 
 
